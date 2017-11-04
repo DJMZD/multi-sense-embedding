28f08a6a6716ef0da3c4528a47d5cd7f23274630
Yong Zuo <spacedogsergei@gmail.com>
2017-11-05 05:24:38 +0900

    Add test for Vocab class

---
+++ test/vocab-test.cc
@@ -0,0 +1,43 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "../mse/vocab.h"
+
+BOOST_AUTO_TEST_SUITE(vocab_test)
+
+BOOST_AUTO_TEST_CASE(constructor_and_setter_test) {
+  Vocab vocab("data/test", 2000);
+  BOOST_CHECK_EQUAL(0, vocab.id("<UNK>"));
+  BOOST_CHECK_EQUAL(1, vocab.id("<s>"));
+  BOOST_CHECK_EQUAL(2, vocab.id("</s>"));
+  BOOST_CHECK_EQUAL(3, vocab.id("the"));
+  BOOST_CHECK_EQUAL(4, vocab.id(","));
+  BOOST_CHECK_EQUAL(5, vocab.id("."));
+  BOOST_CHECK_EQUAL(6, vocab.id("of"));
+  BOOST_CHECK_EQUAL(7, vocab.id("and"));
+  BOOST_CHECK_EQUAL(8, vocab.id("\""));
+  BOOST_CHECK_EQUAL(9, vocab.id("in"));
+  BOOST_CHECK_EQUAL("<UNK>", vocab.word(0));
+  BOOST_CHECK_EQUAL("<s>", vocab.word(1));
+  BOOST_CHECK_EQUAL("</s>", vocab.word(2));
+  BOOST_CHECK_EQUAL("the", vocab.word(3));
+  BOOST_CHECK_EQUAL(",", vocab.word(4));
+  BOOST_CHECK_EQUAL(".", vocab.word(5));
+  BOOST_CHECK_EQUAL("of", vocab.word(6));
+  BOOST_CHECK_EQUAL("and", vocab.word(7));
+  BOOST_CHECK_EQUAL("\"", vocab.word(8));
+  BOOST_CHECK_EQUAL("in", vocab.word(9));
+  BOOST_CHECK_EQUAL(323, vocab.frequency(0));
+  BOOST_CHECK_EQUAL(110, vocab.frequency(1));
+  BOOST_CHECK_EQUAL(110, vocab.frequency(2));
+  BOOST_CHECK_EQUAL(654, vocab.frequency(3));
+  BOOST_CHECK_EQUAL(489, vocab.frequency(4));
+  BOOST_CHECK_EQUAL(411, vocab.frequency(5));
+  BOOST_CHECK_EQUAL(380, vocab.frequency(6));
+  BOOST_CHECK_EQUAL(324, vocab.frequency(7));
+  BOOST_CHECK_EQUAL(270, vocab.frequency(8));
+  BOOST_CHECK_EQUAL(260, vocab.frequency(9));
+  BOOST_CHECK_EQUAL(2003, vocab.size());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
